11bf4a3ffc9369df36c5375002c8113224f72b0f
Marcel Salej <marcel.salej@icloud.com>
2019-10-14 18:27:55 +0200

    removal of network(misunderstood the assignment)

---
+++ Outfit7/Outfit7/Common/Data/Api/User/JSONParsingError.swift
@@ -0,0 +1,14 @@
+//
+//  JSONParsingError.swift
+//  Outfit7
+//
+//  Created by Marcel Salej on 14/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import Foundation
+
+enum JSONParsingError: Error {
+  case noData
+  case parseError
+}
