faa4c7e36cedccd7d1c4d518d76ad36ed9aec631
Marcel Salej <marcel.salej@icloud.com>
2019-10-14 18:27:55 +0200

    removal of network(misunderstood the assignment)

---
+++ Outfit7/Outfit7/Common/Data/Api/User/UserDownloader.swift
@@ -0,0 +1,18 @@
+//
+//  UserDownloader.swift
+//  Beenius
+//
+//  Created by Marcel Salej on 10/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import Foundation
+
+class UserDownloader {
+}
+// MARK: - Downloader implementation
+extension UserDownloader {
+  func loadUsersList(completion: @escaping ((Result<[User], JSONParsingError>) -> Void)) {
+    // load json
+  }
+}
