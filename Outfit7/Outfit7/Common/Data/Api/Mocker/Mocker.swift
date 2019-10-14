fcb4260ebe0168d7288b4afff936405ae11ee0fe
Marcel Salej <marcel.salej@icloud.com>
2019-10-14 18:34:40 +0200

    mocker file setup

---
+++ Outfit7/Outfit7/Common/Data/Api/Mocker/Mocker.swift
@@ -0,0 +1,15 @@
+//
+//  Mocker.swift
+//  Outfit7
+//
+//  Created by Marcel Salej on 14/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import Foundation
+
+class Mocker {
+  static func load<T:Decodable>(from file: String, type: String= "json", configureDecoder((JSONDecoder) -> Void)?) throws -> T {
+    
+  }
+}
